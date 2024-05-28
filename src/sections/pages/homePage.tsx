01c54f3461b9223ec268a2fc8f809940e3e016f1
krishnavthakor <krishnathakor16.kt@gmail.com>
2024-05-27 22:04:15 -0400

    add work experience

--- src/sections/pages/homePage.tsx
+++ src/sections/pages/homePage.tsx
@@ -1,15 +1,25 @@
 // HomePage.js
 import React from "react";
-import { Grid } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import ResponsiveAppBar from "../components/Header";
+import ProfileComponent from "../components/Profile";
+import ExperienceComponent from "../components/Experience";
 
 function HomePage() {
     return (
-        <Grid container>
-            <Grid item xs={12}>
-               <ResponsiveAppBar/>
+        <Container maxWidth={"xl"}>
+            <Grid container>
+                <Grid item xs={12} >
+                    <ResponsiveAppBar/>
+                </Grid>
+                <Grid item xs={12}>
+                    <ProfileComponent/>
+                </Grid>
+                <Grid item xs={12}>
+                    <ExperienceComponent/>
+                </Grid>
             </Grid>
-        </Grid>
+        </Container>
     );
 }
 
